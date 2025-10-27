aea09231f555f91b3a80e545d0bd71c95c1a4bbc
Kaelan Bartlett <228972983+Helodity-M@users.noreply.github.com>
2025-10-27 13:25:43 -0400

    9. Horizontal Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb2d;
     Vector2 moveInput;
     Animator animator;
+
+    [SerializeField] float moveSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,10 +18,13 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb2d.linearVelocityX = moveInput.x;
+        Run();
+    }
+    void Run()
+    {
+        rb2d.linearVelocityX = moveInput.x * moveSpeed;
         animator.SetBool("IsRunning", moveInput.x != 0);
     }
-
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();

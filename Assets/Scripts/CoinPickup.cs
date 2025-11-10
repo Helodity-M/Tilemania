aa6ff7d29b727dfa0f9bea09ebf67700313fe6c5
Kaelan Bartlett <228972983+Helodity-M@users.noreply.github.com>
2025-11-10 12:18:15 -0500

    Update the build

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-
+    [SerializeField] AudioClip pickupSound;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
             Destroy(gameObject);
         }
     }
